93860c31fb077c73dddb34c8f567ac291409470a
Daniel Dinnyes <dinnyesd@gmail.com>
2016-04-27 19:04:44 +0100

    updated configurations
    - moved top level variable customizations to separate custom.el file
    - moved some of the top level customizations into use-package declarations
    - some more customizations in orgmode, like customize templates, and agenda options
    - did some more refactorings in init.el
    - added .bak files to gitignore

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("795d8a0785b16437ee67da091c2c684e4149a12991199c7f5ae4b91637ea0c9c" "71ecffba18621354a1be303687f33b84788e13f40141580fa81e7840752d31bf" default)))
+ '(inhibit-startup-screen t) 
+ '(make-backup-files nil)
+ '(menu-bar-mode nil)
+ '(tool-bar-mode nil))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
