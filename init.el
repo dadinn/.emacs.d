f4d44d9417c5d199e6d6b7a4c8a8e455743db5e6
Daniel Dinnyes <ddinnyes@gmail.com>
2019-05-09 18:39:17 +0100

    automatic setup of exec-path when running on MacOS or X11

--- init.el
+++ init.el
@@ -50,6 +50,11 @@
 (use-package use-package-el-get
   :config (use-package-el-get-setup))
 
+(when (memq window-system '(mac ns x))
+  (use-package exec-path-from-shell
+    :config
+    (exec-path-from-shell-initialize)))
+
 (use-package ido
   :custom
   (ido-enable-flex-matching t)
