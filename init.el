720e1675f2067bcd2f6aef28c2dae2d84ae81499
Daniel Dinnyes <dadinn@besenczy-laptop>
2020-10-17 17:59:27 +0100

    added option require-final-newline
    Always add a newline if missing when saving a file

--- init.el
+++ init.el
@@ -26,6 +26,7 @@
  '(visible-bell t)
  '(visual-line-fringe-indicators
    '(left-curly-arrow right-curly arrow))
+ '(require-final-newline t)
  '(make-backup-files nil)
  '(custom-file "~/.emacs.d/custom.el")
  ;; use-package customizations
