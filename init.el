cd2ab7fab50a815a521f573eb25760dbc118e8f1
Daniel Dinnyes <dadinn@besenczy.net>
2021-12-05 01:46:38 +0000

    disabled in-step org-outline-path completion
    Interferes with ivy/selectrum completion as described in these issues:
    
    https://github.com/raxod502/selectrum/issues/579
    https://github.com/abo-abo/swiper/issues/1254
    https://github.com/abo-abo/swiper/issues/444
    https://emacs.stackexchange.com/questions/37883/hierarchical-org-refile-support-with-ivy
    
    Also, removed obsolete comment about org-completion-use-ido option

--- init.el
+++ init.el
@@ -157,9 +157,7 @@
        (nil . (:tag . "TARGET"))))
    '(org-refile-use-outline-path t)
    '(org-goto-interface 'outline-path-completion)
-   '(org-outline-path-complete-in-steps t)
-   ;; prefer in-steps that ido for refile completion
-   ;; '(org-completion-use-ido t)
+   '(org-outline-path-complete-in-steps nil)
    ;; '(org-reverse-note-order t)
 
    ;; LOGGING
